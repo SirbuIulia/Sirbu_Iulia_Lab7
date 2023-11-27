3205dc2ab66a7f1d96258ed04473512c3098d039
Iulia Sîrbu <iulia.sirbu@stud.ubbcluj.ro>
2023-11-27 18:19:41 +0200

    Laborator 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,28 @@
-﻿namespace Sirbu_Iulia_Lab7;
+﻿using System;
+using Sirbu_Iulia_Lab7.Data;
+using System.IO;
+
+
+namespace Sirbu_Iulia_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
