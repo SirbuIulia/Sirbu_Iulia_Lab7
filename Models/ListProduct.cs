009eb62dedf2e2d6a87d3a4cd6a78149529ac748
Iulia Sîrbu <iulia.sirbu@stud.ubbcluj.ro>
2023-12-05 13:49:07 +0200

    Laborator9

---
+++ Models/ListProduct.cs
@@ -0,0 +1,14 @@
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Sirbu_Iulia_Lab7.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+//using Sirbu_Iulia_Lab7.Models;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Sirbu_Iulia_Lab7.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+    }
+}
+
+
+
+
